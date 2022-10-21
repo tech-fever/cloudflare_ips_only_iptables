66c18d6f71f5cd07c685afc0c06f62a860e589cc
tech-fever <105153585+tech-fever@users.noreply.github.com>
2022-10-21 21:59:13 +0800

    Update restore_cf_read_ips.sh

--- nginx/restore_cf_read_ips.sh
+++ nginx/restore_cf_read_ips.sh
@@ -12,17 +12,17 @@ restore_real_ip_config_file="restore_real_ip.conf"
 
 config_file_full_path="${nginx_conf}/${restore_real_ip_config_file}"
 
-echo "#Cloudflare" >$config_file_full_path;;
+echo "#Cloudflare" > $config_file_full_path;
 for i in `curl https://www.cloudflare.com/ips-v4`; do
-        echo "set_real_ip_from $i;" >>$config_file_full_path;;
+        echo "set_real_ip_from $i;" >> $config_file_full_path;
 done
 for i in `curl https://www.cloudflare.com/ips-v6`; do
-        echo "set_real_ip_from $i;" >>$config_file_full_path;;
+        echo "set_real_ip_from $i;" >> $config_file_full_path;
 done
-echo "" >>$config_file_full_path;;
-echo "# use any of the following two" >>$config_file_full_path;;
-echo "real_ip_header CF-Connecting-IP;" >>$config_file_full_path;;
-echo "#real_ip_header X-Forwarded-For;" >>$config_file_full_path;;
+echo "" >> $config_file_full_path;
+echo "# use any of the following two" >> $config_file_full_path;
+echo "real_ip_header CF-Connecting-IP;" >> $config_file_full_path;
+echo "#real_ip_header X-Forwarded-For;" >> $config_file_full_path;
 
 # 结束
 echo "恢复cloudflare的真实ip成功"
