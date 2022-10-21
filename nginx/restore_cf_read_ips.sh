0bd5ec7d27dd460e34ba2eda6aba61cb5f984acc
tech-fever <105153585+tech-fever@users.noreply.github.com>
2022-10-21 21:55:38 +0800

    Update restore_cf_read_ips.sh

--- nginx/restore_cf_read_ips.sh
+++ nginx/restore_cf_read_ips.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-echo "#Cloudflare" > /etc/nginx/conf.d/cloudflare_ip.conf;
+# nginx 恢复cloudflare的真实ip
+
+# 读取命令行参数中的nginx配置文件路径
+nginx_conf=$1
+restore_real_ip_config_file="restore_real_ip.conf"
+
+config_file_full_path="${nginx_conf}/${restore_real_ip_config_file}"
+
+echo "#Cloudflare" > config_file_full_path;
 for i in `curl https://www.cloudflare.com/ips-v4`; do
-        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/cloudflare_ip.conf;
+        echo "set_real_ip_from $i;" >> config_file_full_path;
 done
 for i in `curl https://www.cloudflare.com/ips-v6`; do
-        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/cloudflare_ip.conf;
+        echo "set_real_ip_from $i;" >> config_file_full_path;
 done
-echo "" >> /etc/nginx/conf.d/cloudflare_ip.conf;
-echo "# use any of the following two" >> /etc/nginx/conf.d/cloudflare_ip.conf;
-echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/cloudflare_ip.conf;
-echo "#real_ip_header X-Forwarded-For;" >> /etc/nginx/conf.d/cloudflare_ip.conf;
+echo "" >> config_file_full_path;
+echo "# use any of the following two" >> config_file_full_path;
+echo "real_ip_header CF-Connecting-IP;" >> config_file_full_path;
+echo "#real_ip_header X-Forwarded-For;" >> config_file_full_path;
